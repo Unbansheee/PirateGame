9d8c29a9f1c61e0276baca155e6857027b67bd5a
Arun Sundaram <26311830+asundr@users.noreply.github.com>
2022-06-19 16:27:21 +0000

    Astar navigation (#18)
    * Game manager holds reference to player and be asked for the list of ports or a random port
    
    * Added ship factions
    
    * Added enemy controller
    
    * Added A* Pathfinding Project
    
    - Source: https://arongranberg.com/astar/download
    
    * Added AStar prefab and enemy prefab Variant
    
    * Moved water from ship o camera
    
    * Made player and enemy variants of ship prefab
    
    * Reorganizing prefab assets

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Faction
+{
+    NONE,
+    PLAYER,
+    MERCHANT,
+    PIRATE,
+    FROG
+}
+
 public class Ship : MonoBehaviour
 {
     private Inventory inventory;
 
     [SerializeField]
     private string shipName;
+    [SerializeField]
+    private Faction faction;
+
+
+    private HealthComponent health;
 
     private void Start()
     {
         //temp
+        inventory.AddMissingItems();
         inventory.RandomizePort();
     }
 
     private void Awake()
     {
+        health = GetComponent<HealthComponent>();
         inventory = GetComponent<Inventory>();
     }
 
